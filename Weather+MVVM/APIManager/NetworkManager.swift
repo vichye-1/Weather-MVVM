6ef3ed7ad7e6f03e3615d4dc26e53e930e0f167f
knockknock <yangban11111@gmail.com>
2024-07-11 09:33:02 +0900

    OpenWeather API 연결 중

--- Weather+MVVM/APIManager/NetworkManager.swift
+++ Weather+MVVM/APIManager/NetworkManager.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() { }
+    
+    func fetchCurrentWeather(api: , completionHandler: @escaping recommendHandler) {
+        print(#function)
+        AF.request(api.endpoint, method: api.method, parameters: api.parameter, encoding: URLEncoding(destination: .queryString), headers: api.header).validate(statusCode: 200..<500).responseDecodable(of: Recommendations.self) { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(value.results, nil)
+            case .failure(let error):
+                completionHandler(nil, error)
+            }
+        }
+    }
+}
--- Weather+MVVM/Model/WeatherDetail.swift
+++ Weather+MVVM/Model/WeatherDetail.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct WeatherResponse: Decodable {
+    let name: String
+    
+}
+
+struct DetailWeather: Decodable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let humidity: Int
+}
