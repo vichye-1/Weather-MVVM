ff05a037309af61fe567a7e3ba70a1520ece2713
knockknock <yangban11111@gmail.com>
2024-07-11 09:31:43 +0900

    openWeather API 연결 중

---
+++ Weather+MVVM/APIManager/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Weather+MVVM
+//
+//  Created by 양승혜 on 7/10/24.
+//
+
+import Foundation
---
+++ Weather+MVVM/Model/WeatherDetail.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherDetail.swift
+//  Weather+MVVM
+//
+//  Created by 양승혜 on 7/10/24.
+//
+
+import Foundation
