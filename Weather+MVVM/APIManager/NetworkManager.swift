866dc0f84f8c57d7cbb4e91566c20dc8d2d22b59
knockknock <yangban11111@gmail.com>
2024-07-11 21:26:16 +0900

    weatherTableView 위치 설정

--- Weather+MVVM/APIManager/NetworkManager.swift
+++ Weather+MVVM/APIManager/NetworkManager.swift
@@ -9,18 +9,18 @@ import Foundation
 import Alamofire
 
 class NetworkManager {
-    static let shared = NetworkManager()
-    private init() { }
-    
-    func fetchCurrentWeather(api: , completionHandler: @escaping recommendHandler) {
-        print(#function)
-        AF.request(api.endpoint, method: api.method, parameters: api.parameter, encoding: URLEncoding(destination: .queryString), headers: api.header).validate(statusCode: 200..<500).responseDecodable(of: Recommendations.self) { response in
-            switch response.result {
-            case .success(let value):
-                completionHandler(value.results, nil)
-            case .failure(let error):
-                completionHandler(nil, error)
-            }
-        }
-    }
+//    static let shared = NetworkManager()
+//    private init() { }
+//    
+//    func fetchCurrentWeather(api: , completionHandler: @escaping recommendHandler) {
+//        print(#function)
+//        AF.request(api.endpoint, method: api.method, parameters: api.parameter, encoding: URLEncoding(destination: .queryString), headers: api.header).validate(statusCode: 200..<500).responseDecodable(of: Recommendations.self) { response in
+//            switch response.result {
+//            case .success(let value):
+//                completionHandler(value.results, nil)
+//            case .failure(let error):
+//                completionHandler(nil, error)
+//            }
+//        }
+//    }
 }
--- Weather+MVVM/WeatherDetail/WeatherDetailViewController.swift
+++ Weather+MVVM/WeatherDetail/WeatherDetailViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class WeatherDetailViewController: BaseViewController {
 
+    private let weatherDetailTableView = {
+        let tableview = UITableView()
+        tableview.backgroundColor = .gray
+        return tableview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    override func configureHierarchy() {
+        view.addSubview(weatherDetailTableView)
+    }
+    
+    override func configureLayout() {
+        weatherDetailTableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 
 }
 
