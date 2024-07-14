04007290759ea5441ebe6815b1cd4ef6a40745d3
knockknock <yangban11111@gmail.com>
2024-07-15 08:02:01 +0900

    hashtagLabel 위치 구현

--- Weather+MVVM/SearchCity/CityTableViewCell.swift
+++ Weather+MVVM/SearchCity/CityTableViewCell.swift
@@ -6,7 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
-class CityTableViewCell: UITableViewCell {
+class CityTableViewCell: BaseTableViewCell {
+    private let hashtagLabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 17)
+        label.text = "#"
+        return label
+    }()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    override func configureHierarchy() {
+        [hashtagLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        hashtagLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+    }
 }
